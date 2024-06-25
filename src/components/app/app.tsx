2abdb9af69cf949d8ba71bd8502ec632a531ee12
ggumiblv <gulnazhakimova70@gmail.com>
2024-06-25 16:24:03 +0200

    router updates

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,7 +2,7 @@ import { ConstructorPage } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, OrderInfo } from '@components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Feed } from '@pages';
 import { Login } from '@pages';
@@ -12,6 +12,7 @@ import { ResetPassword } from '@pages';
 import { Profile } from '@pages';
 import { ProfileOrders } from '@pages';
 import { NotFound404 } from '@pages';
+import { IngredientDetails } from '@components';
 
 const App = () => (
   <div className={styles.app}>
@@ -19,15 +20,32 @@ const App = () => (
     <Router>
       <div className={styles.main}>
         <Routes>
-          <Route path='/' element={<ConstructorPage />} />
-          <Route path='/feed' element={<Feed />} />
+          {/* // TODO поместить в протектед роут */}
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/reset-password' element={<ResetPassword />} />
           <Route path='/profile' element={<Profile />} />
           <Route path='/profile/orders' element={<ProfileOrders />} />
+
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
           <Route path='*' element={<NotFound404 />} />
+          <Route path='/feed/number' element={<OrderInfo />} />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <div className={styles.detailPageWrap}>
+                <p
+                  className={'text text_type_main-large ${styles.detailHeader}'}
+                >
+                  Детали ингридиента
+                </p>
+                <IngredientDetails />
+              </div>
+            }
+          />
+          <Route path='/profile/orders/:number' element={<OrderInfo />} />
         </Routes>
       </div>
     </Router>
