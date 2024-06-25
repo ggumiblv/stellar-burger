8d0fe86f0fdf70d9a5277b7d28eda3c1fb8e3b9e
ggumiblv <gulnazhakimova70@gmail.com>
2024-06-25 15:55:10 +0200

    first base routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,11 +3,38 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader } from '@components';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import { Feed } from '@pages';
+import { Login } from '@pages';
+import { Register } from '@pages';
+import { ForgotPassword } from '@pages';
+import { ResetPassword } from '@pages';
+import { Profile } from '@pages';
+import { ProfileOrders } from '@pages';
+import { NotFound404 } from '@pages';
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Router>
+      <div className={styles.main}>
+        <Routes>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='*' element={<NotFound404 />} />
+        </Routes>
+      </div>
+    </Router>
   </div>
 );
 
